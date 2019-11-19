e4cf697e9edb1eb02e4a13537baf9472dc657207
Milo Bonsall <milo@scrumb.org>
2019-11-18 22:52:44 -0800

    Add user login and home components and routes

--- angular/app/app-routing.module.ts
+++ angular/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './components/user/login/login.component';
+import { HomeComponent } from './components/user/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/user/home/home.component';
+import { LoginComponent } from './components/user/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
