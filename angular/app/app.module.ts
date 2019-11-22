8a015d37904186faa3b86498b666dae48c1684c8
Milo Bonsall <milo@scrumb.org>
2019-11-22 15:33:27 -0800

    refactor app.module.ts and user barrel files

--- angular/app/app.module.ts
+++ angular/app/app.module.ts
@@ -1,49 +1,54 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/user/home/home.component';
+// imports from dependencies                   // alphabetized libs
+import { NgModule }                            from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { JwtInterceptor, ErrorInterceptor } from './shared';
-import { LoginComponent } from './components/user/login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule }                 from '@angular/forms';
+import { BrowserModule }                       from '@angular/platform-browser';
+import { BrowserAnimationsModule }             from '@angular/platform-browser/animations';
 
-import { AlertComponent } from './shared';
-import { AngularMaterialModule } from './material.module';
-import { RegisterComponent } from './components/user/register/register.component';
+// top level and shared imports                // alphabetized directories
+import { AppComponent }                        from './app.component';
+import { AppRoutingModule }                    from './app-routing.module';
+import { AngularMaterialModule }               from './material.module';
+import { AlertComponent }                      from './shared';
+import { JwtInterceptor, ErrorInterceptor }    from './shared';
+
+// component imports
+import { HomeComponent }                       from './components/user';
+import { LoginComponent }                      from './components/user';
+import { RegisterComponent }                   from './components/user';
 import { AdoptTicketComponent } from './components/tech/home/adopt-ticket/adopt-ticket.component';
 import { ResolveTicketComponent } from './components/tech/home/resolve-ticket/resolve-ticket.component';
 import { InviteUserComponent } from './components/tech/home/invite-user/invite-user.component';
 import { AssignTicketComponent } from './components/admin/home/assign-ticket/assign-ticket.component';
 import { PromoteToTechComponent } from './components/admin/home/promote-to-tech/promote-to-tech.component';
 import { PublicComponent } from './components/public/public.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // used to create fake backend
 import { fakeBackendProvider } from './shared';
 
 @NgModule({
   declarations: [
+    // alphabetized
+    AdoptTicketComponent,
     AlertComponent,
     AppComponent,
-    HomeComponent,
-    LoginComponent,
-    RegisterComponent,
-    AdoptTicketComponent,
-    ResolveTicketComponent,
-    InviteUserComponent,
     AssignTicketComponent,
+    HomeComponent,
+    InviteUserComponent,
+    LoginComponent,
     PromoteToTechComponent,
-    PublicComponent
+    PublicComponent,
+    RegisterComponent,
+    ResolveTicketComponent
   ],
   imports: [
+    // alphabetized
     AngularMaterialModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
     HttpClientModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule
+    ReactiveFormsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
---
+++ angular/app/components/user.ts
@@ -0,0 +1,3 @@
+export * from './user/home/home.component';
+export * from './user/login/login.component';
+export * from './user/register/register.component';
