5bd26632590ef6332bc9641cecc23ed814e0a7ad
Milo Bonsall <milo@scrumb.org>
2019-11-22 16:41:06 -0800

    add user create-ticket route

--- angular/app/app-routing.module.ts
+++ angular/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { AuthGuard }                          from './shared';
 
 import { LoginComponent }                      from './components/public';
 import { UserView }                            from './components/user';
+import { CreateTicketComponent }                   from './components/user';
 import { RegisterComponent }                   from './components/user';
 
 const routes: Routes = [
     { path: '', component: UserView, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
+    { path: 'user/create-ticket', component: CreateTicketComponent },
     { path: 'register', component: RegisterComponent },
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
