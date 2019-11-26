829f96b93fb9cc71efaf51f5f26af49fc9cf7120
Milo Bonsall <milo@scrumb.org>
2019-11-25 16:44:11 -0800

    add select dialog and invoke from assign-ticket component

--- angular/app/components/admin/assign-ticket/assign-ticket.component.ts
+++ angular/app/components/admin/assign-ticket/assign-ticket.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }          from '@angular/core';
+import { MatDialog, MatDialogConfig } from "@angular/material";
+import { first }                      from 'rxjs/operators';
+
+import { AuthenticationService,
+         UserService,
+         TicketService  }         from '../../../services';
+
+import { SelectDialogComponent } from '../../../shared';
 
 @Component({
   selector: 'app-assign-ticket',
@@ -7,9 +15,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignTicketComponent implements OnInit {
 
-  constructor() { }
+  currentUser: any;
+  users = [];
+  techs = [];
+  tickets = [];
 
-  ngOnInit() {
+  constructor(
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private ticketService: TicketService,
+    public dialog: MatDialog
+  ) {
+      this.currentUser = this.authenticationService.currentUserValue;
   }
 
+  openSelectDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.data = {
+      subject: 'Tech',
+      values: this.loadAllTechs()
+    };
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    const dialogRef = this.dialog.open(SelectDialogComponent, dialogConfig);
+
+    dialogRef.afterClosed().subscribe(
+        data => console.log("Dialog output:", JSON.stringify(data))
+    );
+  }
+  ngOnInit() {
+      this.loadAllUsers();
+      this.loadAllTickets();
+  }
+
+  private loadAllUsers() {
+      this.userService.getAll()
+          .pipe(first())
+          .subscribe(users => this.users = users);
+  }
+
+  private loadAllTickets() {
+      this.ticketService.getAll()
+          .pipe(first())
+          .subscribe(tickets => this.tickets = tickets);
+  }
+
+  private loadAllTechs() {
+      return this.users.filter(function (e) {
+        return e.tech == true;
+      });
+  }
 }
